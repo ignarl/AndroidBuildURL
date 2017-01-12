24d2c942ec4cdec1c97b562760d4e659e3e39362
ignarl <ignarl@gmail.com>
2017-01-13 10:07:09 +1100

    Update BuildURL.java

--- BuildURL.java
+++ BuildURL.java
@@ -1,19 +1,28 @@
-     public static URL buildUrl(String locationQuery) {
-+        Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
-+                .appendQueryParameter(QUERY_PARAM, locationQuery)
-+                .appendQueryParameter(FORMAT_PARAM, format)
-+                .appendQueryParameter(UNITS_PARAM, units)
-+                .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
-+                .build();
-+
-+        URL url = null;
-+        try {
-+            url = new URL(builtUri.toString());
-+        } catch (MalformedURLException e) {
-+            e.printStackTrace();
-+        }
-+
-+        Log.v(TAG, "Built URI " + url);
-+
-+        return url;
-     }
+/*  final static String QUERY_PARAM = "q";
+    final static String LAT_PARAM = "lat";
+    final static String LON_PARAM = "lon";
+    final static String FORMAT_PARAM = "mode";
+    final static String UNITS_PARAM = "units";
+    final static String DAYS_PARAM = "cnt"; */     
+
+
+
+public static URL buildUrl(String locationQuery) {
+        Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon() //depends on how the url needs to be built
+                .appendQueryParameter(QUERY_PARAM, locationQuery)
+                .appendQueryParameter(FORMAT_PARAM, format)
+                .appendQueryParameter(UNITS_PARAM, units)
+                .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Log.v(TAG, "Built URI " + url);
+
+        return url;
+    }
